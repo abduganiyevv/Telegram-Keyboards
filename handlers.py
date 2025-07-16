6391534fd6c7b475ab382c2a21964a9c55091b0e
djumanov <djumanovdev@gmail.com>
2025-07-16 21:57:14 +0500

    inline keyboard

--- handlers.py
+++ handlers.py
@@ -1,15 +1,14 @@
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext
 
 
 def start(update: Update, context: CallbackContext):
-    bot = context.bot
+    update.message.reply_text("salom, \n\n/keyboard\n/inline_keyboard")
 
-    user = update.effective_user
 
-    bot.send_message(
-        chat_id=user.id,
-        text="Salom Botga xush kelibsiz.",
+def send_keyboard(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text="Keyboard.",
         reply_markup=ReplyKeyboardMarkup(
             keyboard=[
                 [KeyboardButton('Mahsulotlar'), KeyboardButton("Buyurtmalarim")],
@@ -19,3 +18,13 @@ def start(update: Update, context: CallbackContext):
         )
     )
 
+
+def send_inline_keyboard(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text="Inline Keyboard.",
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton('button 1', callback_data="btn1"), InlineKeyboardButton('button 2', callback_data='btn2')]
+            ]
+        )
+    )
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     # command handler
     dispatcher.add_handler(CommandHandler("start", handlers.start))
+    dispatcher.add_handler(CommandHandler("keyboard", handlers.send_keyboard))
+    dispatcher.add_handler(CommandHandler("inline_keyboard", handlers.send_inline_keyboard))
 
     # start bot
     updater.start_polling()
