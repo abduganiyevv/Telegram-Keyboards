7579d7a446d40452764bb13680f4d5d786362f7d
abduganiyevv <firdavsabduganiyev07@gmail.com>
2025-07-18 19:32:41 +0500

    bot_yaratildi

--- handlers.py
+++ handlers.py
@@ -3,7 +3,7 @@ from telegram.ext import CallbackContext
 
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("salom, \n\n/keyboard\n/inline_keyboard")
+    update.message.reply_text("Salom! Darslar kerak bo‘lsa /keyboard tugmasini bosing.")
 
 
 def send_keyboard(update: Update, context: CallbackContext):
@@ -11,23 +11,80 @@ def send_keyboard(update: Update, context: CallbackContext):
         text="Keyboard.",
         reply_markup=ReplyKeyboardMarkup(
             keyboard=[
-                [KeyboardButton('Mahsulotlar'), KeyboardButton("Buyurtmalarim")],
-                [KeyboardButton('Contact', request_contact=True), KeyboardButton("Location", request_location=True)],
+                [KeyboardButton('Frontend dasturlar'), KeyboardButton("Backend dasturlar"),KeyboardButton('No Code dasturlari')],
+                [KeyboardButton('Dizaynerlik darslari'),KeyboardButton('Database'),KeyboardButton('Android')],
+                [KeyboardButton("Bog‘lanish")],
             ],
             resize_keyboard=True,
         )
     )
 
 
-def send_inline_keyboard(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        text="Inline Keyboard.",
-        reply_markup=InlineKeyboardMarkup(
-            inline_keyboard=[
-                [InlineKeyboardButton('button 1', url="https://kun.uz/"), InlineKeyboardButton('button 2', callback_data='btn2')]
-            ]
+def handle_message(update, context):
+    text = update.message.text
+
+    if text == 'Frontend dasturlar':
+        update.message.reply_text(
+            "---Frontend darslari---\n" \
+            "CSS, SASS, JavaScript, React, HTML darslari\n"
+            "👇👇👇\n"
+            " https://www.youtube.com/playlist?list=PLBoBu8c5jyTH790_UBbhtF0ZGnW8Alup5",
         )
-    )
+    elif text == 'Backend dasturlar':
+        update.message.reply_text(
+            "---Backend darslari---\n"
+            "C++, PYTHON darslari\n"
+            "👇👇👇\n"
+            "https://www.youtube.com/playlist?list=PLwsopmzfbOn_1XLqRYV7gu8ozcYvPEqmd"
+        )
+    elif text == 'No Code dasturlari':
+        update.message.reply_text(
+            "---No Code darslari---\n"
+            "Telegram bot yaratish, Sayt yaratish darslari\n"
+            "👇👇👇\n"
+            "https://www.youtube.com/playlist?list=PLtJ-ufoDnA2DQIO-UBOIWiQffp7DC9xn5"
+
+        )
+    elif text == 'Dizaynerlik darslari':
+        update.message.reply_text(
+            "---Dizayn darslari---\n"
+            "Figma, Canva Grafik dizayn darslari\n"
+            "👇👇👇\n"
+            "https://www.youtube.com/playlist?list=PLQWhzdFNj2CeAI0EFe0r5_vYaJUqMQxd-"
+        )
+    elif text == 'Database':
+        update.message.reply_text(
+            "---Database darslari---\n"
+            "PostgreSQL, MySQL, MongoDB darslari\n"
+            "👇👇👇\n"
+            "https://www.youtube.com/playlist?list=PLcVkxa4hDY3HMSLeg6QOLftJUhLX2JJPe"
+
+        )
+    elif text == 'Android':
+        update.message.reply_text(
+            "---Android darslari---\n"
+            "Kotlin, Java va Android darslari\n"
+            "👇👇👇\n"
+            "https://www.youtube.com/playlist?list=PL2MlJHnfSUYrkWMLoq6-rs6C6vkXuAZSj"
+        )
+    elif text == 'Bog‘lanish':
+        update.message.reply_text("Admin bilan bog‘lanish:\n" \
+        "" \
+        "https://t.me/firdavsbek_abduganiyev")
+    else:
+        update.message.reply_text("Iltimos, menyudan tanlang.")
+
+
+
+# def send_inline_keyboard(update: Update, context: CallbackContext):
+#     update.message.reply_text(
+#         text="Inline Keyboard.",
+#         reply_markup=InlineKeyboardMarkup(
+#             inline_keyboard=[
+#                 [InlineKeyboardButton('button 1', url="https://kun.uz/"), InlineKeyboardButton('button 2', callback_data='btn2')]
+#             ]
+#         )
+#     )
 
 
 def send_products(update: Update, context: CallbackContext):
--- main.py
+++ main.py
@@ -7,19 +7,17 @@ def main():
     updater = Updater(token=TOKEN)
     dispatcher = updater.dispatcher
 
-    # command handler
+
     dispatcher.add_handler(CommandHandler("start", handlers.start))
     dispatcher.add_handler(CommandHandler("keyboard", handlers.send_keyboard))
-    dispatcher.add_handler(CommandHandler("inline_keyboard", handlers.send_inline_keyboard))
+    dispatcher.add_handler(MessageHandler(Filters.text, handlers.handle_message))
+    # dispatcher.add_handler(CommandHandler("inline_keyboard", handlers.send_inline_keyboard))
 
-    # message handler
     dispatcher.add_handler(MessageHandler(Filters.text("Mahsulotlar"), handlers.send_products))
-    # dispatcher.add_handler(MessageHandler(Filters.text("Buyurtmalarim"), handlers.)) # add handler
 
-    # callbackquery handler
-    dispatcher.add_handler(CallbackQueryHandler(callback=handlers.handle_btn2, pattern='btn2'))
+    # dispatcher.add_handler(CallbackQueryHandler(callback=handlers.handle_btn2, pattern='btn2'))
 
-    # start bot
+   
     updater.start_polling()
     updater.idle()
 
